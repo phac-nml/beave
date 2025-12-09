37d689b85f6d14b642f6a059624724aa05982433
Matthew Wells <mattheww9514@gmail.com>
2025-12-09 13:06:08 -0600

    fixed issue when profiles amount is less than threads

--- src/main.cpp
+++ src/main.cpp
@@ -292,7 +292,7 @@ std::string read_profiles(const char *file, std::vector<DMPair> &data,
 // process they can then all write to the output matrix
 std::vector<size_t> get_thread_ranges(size_t threads, size_t data_size) {
   std::vector<size_t> ranges;
-  if (threads <= 1) {
+  if (threads <= 1 || data_size <= threads) {
     threads = 1;
     ranges.push_back(0);
     ranges.push_back(data_size);
