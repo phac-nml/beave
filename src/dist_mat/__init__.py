92e13302df6296877f354a50aa86589af7f3fb94
Matthew Wells <mattheww9514@gmail.com>
2026-02-20 16:31:18 -0600

    updated source files so that allowing the library to builid run and install

---
+++ src/dist_mat/__init__.py
@@ -0,0 +1,3 @@
+from .dist_mat_ext import calc_dists, __doc__
+
+__all__ = ("calc_dists", "__doc__")
--- src/dist_mat/test2.py
+++ src/dist_mat/test2.py
@@ -3,11 +3,11 @@ import numpy as np
 import scipy as sp
 import dist_mat as dm
 
-
 array = np.array(
     [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 9]],
     dtype=np.uint32,
 )
+
 
 sp_mat = sp.spatial.distance.pdist(array, metric="hamming")
 print(sp_mat)
