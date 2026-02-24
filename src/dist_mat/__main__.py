a1fab702de4d3a77f5c91bb65500a76ed9b9b0a3
Matthew Wells <mattheww9514@gmail.com>
2026-02-24 12:27:12 -0600

    added mcluster demo

---
+++ src/dist_mat/__main__.py
@@ -0,0 +1,157 @@
+import argparse
+import sys
+import os
+import pathlib as p
+from enum import StrEnum
+
+from dist_mat import mcluster
+
+
+class Commands(StrEnum):
+    MCLUSTER = "mcluster"
+
+
+class DistanceMetrics(StrEnum):
+    WARD = "ward"
+    SINGLE = "single"
+    AVERAGE = "average"
+    CENTROID = "centroid"
+    MEDIAN = "median"
+    COMPLETE = "complete"
+
+
+def main():
+    # specify global arguments shared here
+    parent_parser = argparse.ArgumentParser(
+        add_help=False,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    number_of_cores_default = 1
+    if cpu_count := os.cpu_count():
+        number_of_cores_default = cpu_count // 2
+
+    parent_parser.add_argument(
+        "--n-threads",
+        "-n",
+        help="Specify the number of threads to be used. [default %(default)d]",
+        type=int,
+        default=number_of_cores_default,
+    )
+    parent_parser.add_argument(
+        "--delimiter",
+        "-d",
+        help="Input alleles delimiter. [default \\t]",
+        type=str,
+        default="\t",
+    )
+
+    parser = argparse.ArgumentParser(
+        "Example program",
+        description="A quick proof of concept of generic utilities for nomenclature assignment.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        parents=[parent_parser],
+    )
+
+    parser.add_argument(
+        "--version", "-v", help="Print version and exit.", action="store_true"
+    )
+
+    subparsers = parser.add_subparsers(
+        help="Select a program to run.",
+        dest="command",
+    )
+
+    # Mcluster args
+    parser_mcluster = subparsers.add_parser(
+        Commands.MCLUSTER, help="Run denovo clustering.", parents=[parent_parser]
+    )
+
+    parser_mcluster.add_argument(
+        "--input", "-i", help="Input alleles.", type=p.Path, required=True
+    )
+
+    parser_mcluster.add_argument(
+        "--tree-output",
+        "-t",
+        help="Output tree name. [default %(default)s]",
+        type=p.Path,
+        required=False,
+        default="clusters.nwk",
+    )
+
+    parser_mcluster.add_argument(
+        "--cluster-output",
+        "-l",
+        help="Output clusters file. [default %(default)s]",
+        type=p.Path,
+        required=False,
+        default="clusters.tsv",
+    )
+
+    parser_mcluster.add_argument(
+        "--thresholds",
+        "-p",
+        help="List of threshold values to use, can be Float or Int",
+        nargs="+",
+        required=True,
+        action="extend",
+        type=float,
+    )
+
+    parser_mcluster.add_argument(
+        "--method",
+        "-m",
+        default=DistanceMetrics.AVERAGE.value,
+        help="Linkage method to use. [default: %(default)s]",
+        choices=[i.value for i in DistanceMetrics],
+    )
+
+    parser_mcluster.add_argument(
+        "--columns",
+        "-k",
+        help="A file containing a list of columns to subset from the allele profiles.",
+        type=p.Path,
+        required=False,
+    )
+
+    parser_mcluster.add_argument(
+        "--count-missing",
+        "-c",
+        help="Count missing values as differences.",
+        action="store_true",
+    )
+
+    parser_mcluster.add_argument(
+        "--scaled",
+        "-s",
+        help="Compute the scaled distance.",
+        action="store_true",
+    )
+
+    args = parser.parse_args(sys.argv[1:])
+
+    if args.version:
+        print("0.0.1")
+        sys.exit()
+
+    match args.command:
+        case Commands.MCLUSTER:
+            mcluster.mcluster(
+                args.input,
+                args.delimiter,
+                args.thresholds,
+                args.method,
+                args.n_threads,
+                args.columns,
+                args.count_missing,
+                args.scaled,
+                args.tree_output,
+                args.cluster_output,
+            )
+        case _:
+            print("nothing selected.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/dist_mat/mcluster.py
@@ -0,0 +1,226 @@
+"""
+Re-implementation of mcluster
+"""
+
+from os import link
+import sys
+import pathlib as p
+
+import dist_mat as dm
+import scipy as sp
+import polars as pl
+import numpy as np
+from numpy import typing as npt
+
+
+def _scipy_tree_to_newick_list(node, newick, parentdist, leaf_names):
+    """Construct Newick tree from SciPy hierarchical clustering ClusterNode
+
+    This is a recursive function to help build a Newick output string from a scipy.cluster.hierarchy.to_tree input with
+    user specified leaf node names.
+
+    Notes:
+        This function is meant to be used with `to_newick`
+
+    Args:
+        node (scipy.cluster.hierarchy.ClusterNode): Root node is output of scipy.cluster.hierarchy.to_tree from hierarchical clustering linkage matrix
+        parentdist (float): Distance of parent node of `node`
+        newick (list of string): Newick string output accumulator list which needs to be reversed and concatenated (i.e. `''.join(newick)`) for final output
+        leaf_names (list of string): Leaf node names
+
+    Returns:
+        (list of string): Returns `newick` list of Newick output strings
+    """
+    if node.is_leaf():
+        return newick + [f"{leaf_names[node.id]}:{parentdist - node.dist}"]
+
+    if len(newick) > 0:
+        newick.append(f"):{parentdist - node.dist}")
+    else:
+        newick.append(");")
+    newick = _scipy_tree_to_newick_list(node.get_left(), newick, node.dist, leaf_names)
+    newick.append(",")
+    newick = _scipy_tree_to_newick_list(node.get_right(), newick, node.dist, leaf_names)
+    newick.append("(")
+    return newick
+
+
+def to_newick(tree, leaf_names) -> str:
+    """Newick tree output string from SciPy hierarchical clustering tree
+
+    Convert a SciPy ClusterNode tree to a Newick format string.
+    Use scipy.cluster.hierarchy.to_tree on a hierarchical clustering linkage matrix to create the root ClusterNode for the `tree` input of this function.
+
+    Args:
+        tree (scipy.cluster.hierarchy.ClusterNode): Output of scipy.cluster.hierarchy.to_tree from hierarchical clustering linkage matrix
+        leaf_names (list of string): Leaf node names
+
+    Returns:
+        (string): Newick output string
+    """
+    newick_list = _scipy_tree_to_newick_list(tree, [], tree.dist, leaf_names)
+    return "".join(newick_list[::-1])
+
+
+def read_input_profiles(
+    input: p.Path, columns_keep: p.Path | None, delimiter: str, threads: int
+) -> pl.DataFrame:
+    """
+    Ingest the allelic profiles and perform any filtering and conversions required.
+    """
+
+    profiles = pl.read_csv(
+        input, separator=delimiter, n_threads=threads, has_header=True
+    )
+
+    if columns_keep is not None:
+        columns_to_keep: set[str] | None = None
+        with columns_keep.open("r") as ck:
+            columns_to_keep = set([line.strip() for line in ck.readlines()])
+
+        sample_col = profiles.columns[0]
+        profile_cols = set(profiles.columns[1:])  # keep all columns but first
+        columns_keep_set = list(profile_cols & columns_to_keep)
+        profiles = profiles.select(sample_col, *columns_keep_set)
+
+    ## Can add additional filtering logic here
+    return profiles
+
+
+def prep_data(profiles: pl.DataFrame) -> npt.NDArray:
+    """
+    Prepare profiles for ingestion by the the calc_dists function of dist_mat.
+    """
+
+    data_columns = profiles.columns[1:]  # only apply functions to test columns
+    missing_value = np.uint32(0)
+    mapping_replace = {
+        "?": missing_value,
+        " ": missing_value,
+        "-": missing_value,
+        "": missing_value,
+        "_": missing_value,
+    }  # mappings to replace fields with zeroes
+    profiles = profiles.with_columns(
+        [pl.col(i).replace(mapping_replace) for i in data_columns]
+    )
+
+    profiles = profiles.with_columns(
+        [
+            pl.when(pl.col(i) != str(missing_value))
+            .then(pl.col(i).hash(42, 42, 42, 42))
+            .otherwise(pl.lit(0))
+            for i in data_columns
+        ]
+    )
+
+    profiles_numpy = profiles.select([pl.col(i) for i in data_columns]).to_numpy()
+    profiles_numpy = profiles_numpy.astype(
+        np.uint32
+    )  # convert hashes to 32 bit representation for dist_mat
+    return profiles_numpy
+
+
+def compute_dists(
+    profiles: pl.DataFrame, count_missing: bool, scaled: bool, threads: int
+) -> npt.NDArray:
+    """
+    Compute the 1D array required by scipy for generation of the linkage matrix.
+    """
+    prepared_profiles = prep_data(profiles)
+    distances = dm.calc_dists(prepared_profiles, threads, scaled, count_missing)
+    return distances
+
+
+def comp_linkage_matrix(
+    profiles_computed: npt.NDArray, linkage_method: str
+) -> npt.NDArray:
+    linkage = sp.cluster.hierarchy.linkage(profiles_computed, method=linkage_method)
+    return linkage
+
+
+def assign_clusters(
+    linkage: npt.NDArray, thresholds: list[int | float], labels: list[str]
+):
+    """
+    Roll the new cluster membership code.
+
+    Thresholds are assumed to be sorted on input
+    """
+
+    cluster_members = pl.Schema(
+        [("Sample", pl.String), *[(str(k), pl.UInt32) for k in thresholds]]
+    )
+    data_to_populate = [pl.Series(name="SampleID", values=labels)]
+    cols_concat = []
+
+    for threshold in thresholds:
+        col_name = str(threshold)
+        data_to_populate.append(
+            pl.Series(
+                name=col_name,
+                values=sp.cluster.hierarchy.fcluster(
+                    linkage, threshold, criterion="distance"
+                ),
+                dtype=pl.UInt32,
+            )
+        )
+        cols_concat.append(col_name)
+    outputs = pl.DataFrame(data_to_populate, orient="col")
+    outputs = outputs.with_columns(
+        pl.concat_str(cols_concat, separator=".").alias("denovo_address")
+    )
+    return outputs
+
+
+def mcluster(
+    input: p.Path,
+    delimiter: str,
+    thresholds: list[int | float],
+    methods: str,
+    n_threads: int,
+    columns: p.Path | None,
+    count_missing: bool,
+    scaled: bool,
+    tree_output: p.Path,
+    cluster_outputs: p.Path,
+    *args,
+    **kwargs,
+):
+    """
+    Main runner function for mcluster.
+    """
+
+    profiles = read_input_profiles(input, columns, delimiter, n_threads)
+    print("Ingested profiles")
+    distances = compute_dists(profiles, count_missing, scaled, n_threads)
+    print("Computed distances")
+    linkages = comp_linkage_matrix(distances, methods)
+    print("Computed linkage matrix")
+    thresholds.sort(reverse=True)
+
+    sample_names = profiles.select(pl.nth(0)).to_series().to_list()
+    # write out the tree
+    cluster_memberships = assign_clusters(linkages, thresholds, sample_names)
+    print("assigned clusters")
+
+    sys.setrecursionlimit(4000)  # raise recursion limit for generating the tree
+    tree = sp.cluster.hierarchy.to_tree(linkages)
+    newick = to_newick(tree, sample_names)
+    with tree_output.open("w") as to:
+        to.write(newick)
+    print("Wrote out newick")
+
+    # using polars for this output would greatly speed it up and simplify the code
+
+    cluster_memberships.write_csv(
+        cluster_outputs, separator=delimiter, include_header=True
+    )
+    print("wrote out cluster memberships")
+    # with cluster_outputs.open("w") as co:
+    #    co.write("\t".join(["sample_id", *[str(i) for i in thresholds], "\n"]))
+    #    for k, v in cluster_memberships.items():
+    #        co.write("\t".join([k, *[str(i) for i in v], "\n"]))
+
+    # write out the threshold groupoings
+    #
