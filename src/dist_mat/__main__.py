693cbdaf2dae98cf6c13eb6552dd0ffba0639258
Matthew Wells <mattheww9514@gmail.com>
2026-03-30 16:34:18 -0500

    updated changes md5 hashes still reviewing examples

--- src/dist_mat/__main__.py
+++ src/dist_mat/__main__.py
@@ -67,7 +67,7 @@ def percentage_range(float_input: str) -> float:
         )
         logger.critical(error_message)
         raise ValueError(error_message)
-    return converted_float / 100.0  # convert percentage to decimal
+    return converted_float / 100.0  # convert percentage to decimal fraction
 
 
 def cluster_threshold(float_input: str) -> float:
